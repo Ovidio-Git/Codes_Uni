b795ac83598107a1c84eb3f60a73a53dc2a69328
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-10 16:46:05 -0500

    exercise 6 class 10/03/21

---
+++ desing_micro/ejercicio06.py
@@ -0,0 +1,17 @@
+
+
+def num():
+	number = int(input('Ingrese un numero: '))
+	if (number%2 == 0):
+		print('El numero es par')
+	else:
+		print('El numero es impar')
+
+
+def run():
+	num()	
+
+
+
+if __name__ == '__main__':
+	run()
