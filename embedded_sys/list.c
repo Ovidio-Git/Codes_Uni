2e3cda8c29327c86515d887b33bd7d6509c1fe36
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-16 18:23:59 -0500

    list 5%

---
+++ embedded_sys/list.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct{
+	char Dato; 			 // Dato a almacenar en la lista
+	struct Nodo *ptrnext; // Autoreferenciado de la estrucutra
+} Nodo;
+
+
+
+int main (){
+
+
+	return 0;
+}
