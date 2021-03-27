f808022d4a18df2d01e79fdb6d3764c618bb0b4c
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-26 22:37:29 -0500

    updates tables and inset files of insert_data

---
+++ MySQL/Insert_in_table.sql
@@ -0,0 +1,24 @@
+
+
+INSERT INTO authors(author_id, name, nationality)
+VALUES(null,'Juan Rulfo','MEX');
+
+INSERT INTO authors(name, nationality)
+VALUES('Gabriel Gracia Marquez', 'COL');
+
+INSERT INTO authors()
+VALUES(null, 'Juan Grabriel Vasquez', 'COL');
+
+INSERT INTO authors(name, nationality)
+VALUES
+('Julio Cortazar', 'ARG'), 
+('Isabel Allende','CHI'),
+('Octavio Paz', 'MEX'),
+('Juan Carlos Onetti', 'URU'),
+('Pablo Neruda','CHI'),
+('Mario Vargas LLosa', 'PER'),
+('Mario Benedetti', 'URU'),
+('Paulo Coelho', 'BRA');
+
+ 
+
