692ebc21fcc6db621ee67cdb45c87da8147639b5
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-04-12 09:12:38 -0500

    second commit

--- embedded_sys/Taller1.c
+++ embedded_sys/Taller1.c
@@ -8,18 +8,22 @@
 // SOCK_STREAM -> TCP 
 // SOCK_DGRAM -> UDP
 
+
+
+
 int main(){
     
     int connection = 0;
     int socket_id = 0;
-    char msg[] ="Hello sir\n\r";
+    char buffer[500];
+    char msg[] ="GET /index.html HTTP/1.1\n\r";
     socket_id = socket(PF_INET, SOCK_STREAM, 0);
 
     // Server parameters   AWS IP Public= 18.222.148.97
     struct sockaddr_in server;
     server.sin_family = AF_INET ;  // protocol
-    server.sin_addr.s_addr = inet_addr("157.240.6.35"); // server ip
-    server.sin_port = htons(12000); // connection port
+    server.sin_addr.s_addr = inet_addr("172.217.173.196"); // server ip
+    server.sin_port = htons(443); // connection port
     // htons(port number) convert to 6 bit format 
     
     
@@ -31,7 +35,16 @@ int main(){
     }
     printf("[CONNECTION SUCCESS]\r\n");
     send(socket_id, msg, sizeof(msg), 0); 
+    recv(connection,buffer,sizeof(buffer),0);
+    for (int i = 0; i < 5; i++)
+    {
+         printf(recv(connection,buffer,sizeof(buffer),0));
+         printf(buffer[i]);
+    }
+    
+   
     close(socket_id);
 
     return (0);
 
+}
